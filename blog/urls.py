f4fca4ae85a15e6fed45c1cbfdf312b8c601b02d
akramfdk <akramjeet.singh@gmail.com>
2025-07-27 18:04:01 +1000

    urls and views commit

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="starting-page"),
     path("posts", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.post_detail, name="post-detail-page"),  # /posts/my-first-post
+    path("posts/<slug:slug>", views.post_detail,
+         name="post-detail-page"),  # /posts/my-first-post
 
 ]
