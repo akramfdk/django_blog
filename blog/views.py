da2d8a7374c8b57a6ffd03e2442a2593badbeeb8
akramfdk <akramjeet.singh@gmail.com>
2025-08-06 23:35:12 +1000

    Changed all function views to class based views

--- blog/views.py
+++ blog/views.py
@@ -3,102 +3,51 @@ from datetime import date
 
 from .models import Post
 
-# posts = [
-#     {
-#         "slug": "hike-in-the-mountains",
-#         "image": "mountains.jpg",
-#         "author": "Akram",
-#         "date": date(2025, 7, 21),
-#         "title": "Mountain Hiking",
-#         "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-#         "content": """
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-#         """
-#     },
-#     {
-#         "slug": "programming-is-fun",
-#         "image": "coding.jpg",
-#         "author": "Akram",
-#         "date": date(2025, 3, 10),
-#         "title": "Programming Is Great!",
-#         "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-#         "content": """
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-#         """
-#     },
-#     {
-#         "slug": "into-the-woods",
-#         "image": "woods.jpg",
-#         "author": "Akram",
-#         "date": date(2024, 8, 5),
-#         "title": "Nature At Its Best",
-#         "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-#         "content": """
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-#         """
-#     }
-# ]
-
-# def get_date(post):
-#     return post['date']
-
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3]
-    # sorted_posts = sorted(posts, key=get_date)
-    # latest_posts = sorted_posts[-3:]
-    return render(request, "blog/index.html", context = {
-        "latest_posts": latest_posts,
-    })
+class StartingPageView(ListView):
+    model = Post
+    template_name = "blog/index.html"
+    ordering = ["-date"]
+    context_object_name = "latest_posts"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
 
-def all_posts(request):
-    posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/all-posts.html", context={
-        "all_posts": posts,
-    })
+class AllPostsView(ListView):
+    model = Post
+    template_name = "blog/all-posts.html"
+    ordering = ["-date"]
+    context_object_name = "all_posts"
 
 
-def post_detail(request, slug):
-    # req_post = None
-    # for post in posts:
-    #     if slug == post["slug"]:
-    #         req_post = post
 
-    req_post = get_object_or_404(Post, slug=slug)
-    # req_post = next(post for post in posts if post['slug'] == slug)
-    return render(request, "blog/post-detail.html", context={
-        "post": req_post,
-        "post_tags": req_post.tags.all()
-    })
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post-detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["post_tags"] = self.object.tags.all()
+        return context
+
+    # slug_field = "slug"
+    # slug_url_kwarg = "slug"
+
+# def post_detail(request, slug):
+#     # req_post = None
+#     # for post in posts:
+#     #     if slug == post["slug"]:
+#     #         req_post = post
+
+#     req_post = get_object_or_404(Post, slug=slug)
+#     # req_post = next(post for post in posts if post['slug'] == slug)
+#     return render(request, "blog/post-detail.html", context={
+#         "post": req_post,
+#         "post_tags": req_post.tags.all()
+#     })
