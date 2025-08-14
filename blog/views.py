979739e20d01ba32e6127e7f1324e9051fe7ba23
akramfdk <akramjeet.singh@gmail.com>
2025-08-14 11:33:34 +1000

    added functionality for users to comment on a form and store posts for later within seession

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from datetime import date
 
 from .models import Post
 
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
+from django.views import View
+from .forms import CommentForm
 
 # Create your views here.
 class StartingPageView(ListView):
@@ -26,15 +30,92 @@ class AllPostsView(ListView):
 
 
 
-class PostDetailView(DetailView):
-    model = Post
-    template_name = "blog/post-detail.html"
+class PostDetailView(View):
+    # model = Post
+    # template_name = "blog/post-detail.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context["post_tags"] = self.object.tags.all()
+    #     context["comment_form"] = CommentForm()
+    #     return context
 
-        context["post_tags"] = self.object.tags.all()
-        return context
+    def is_stored_post(self, request, post_id):
+
+        stored_posts = request.session.get("stored_posts")
+        is_saved_for_later = False
+        if stored_posts and post_id in stored_posts:
+            is_saved_for_later = True
+
+        return is_saved_for_later
+    
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        
+        context={
+                "post": post,
+                "post_tags": post.tags.all(),
+                "comment_form": CommentForm(),
+                "comments": post.comments.all().order_by("-id"),
+                "saved_for_later": self.is_stored_post(request, post.id),
+            }
+
+        return render(request, "blog/post-detail.html", context) 
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+        
+        context={
+                "post": post,
+                "post_tags": post.tags.all(),
+                "comment_form": comment_form,
+                "comments": post.comments.all().order_by("-id"),
+                "saved_for_later": self.is_stored_post(request, post.id),
+        }
+        return render(request, "blog/post-detail.html", context)
+    
+
+class ReadLaterView(View):
+    def get(self, request):
+        stored_posts = request.session.get("stored_posts")
+
+        context = {}
+
+        if not stored_posts:
+            context["posts"] = []
+            context["has_posts"] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context["posts"] = posts
+            context["has_posts"] = True
+
+        return render(request, "blog/stored-posts.html", context)
+
+
+
+    def post(self, request):
+        stored_posts = request.session.get("stored_posts")
+
+        if not stored_posts:
+            stored_posts = []
+
+        post_id = int(request.POST["post_id"])
+
+        if post_id not in stored_posts:
+            stored_posts.append(post_id)
+        else:
+            stored_posts.remove(post_id)
+
+        request.session["stored_posts"] = stored_posts
+        
+        return HttpResponseRedirect("/")
 
     # slug_field = "slug"
     # slug_url_kwarg = "slug"
